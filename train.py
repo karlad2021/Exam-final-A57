d74663f02103706f73d9dbd0ec85a235a909364d
karlad2021 <78771342+karlad2021@users.noreply.github.com>
2021-03-19 20:55:55 -0400

    Update train.py

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ y = df.iloc[:, 0]
 # In[19]:
 
 
-# Split des données
+# #Split des données
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=44)
 
